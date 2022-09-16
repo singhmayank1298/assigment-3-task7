ea21e1a5aa9a402bbbf2ca49afb72414f7db913e
singhmayank1298 <108435481+singhmayank1298@users.noreply.github.com>
2022-09-17 01:03:05 +0530

    task9 context Api

---
+++ task-7-of-assigment-3/src/components/store/auth-contex.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const AuthContext= React.createContext(); // it will make a object it take default state ,we can pass any data type
+                                         // default value is useable when you does not creat Provider
+export default AuthContext
